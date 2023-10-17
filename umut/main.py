05918bd51491e31dd7836b431ae51f003a712ea4
umutulupinar <umutulupinarrr@gmail.com>
2023-10-18 01:32:31 +0300

    files are seperating and roadmap is added

---
+++ umut/main.py
@@ -0,0 +1,9 @@
+def print_conversation(name,number):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, my name is {name} and my number is {number}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_conversation('umut','009')
