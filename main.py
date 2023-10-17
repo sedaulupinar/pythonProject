93afac15aedb9ed7107828cb922c08a01051bbde
sedaulupinar <seda.ulupinarrrr@gmail.com>
2023-10-18 01:02:00 +0300

    initial

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, my name is {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def print_no(number):
+    print(f'my number is, {number}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Seda')
+    print_no('007')
+    print_hi('Umut')
+    print_no('009')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
