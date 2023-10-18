e261cd07a331e98bb8ce2ebea78b93e84f77b6cb
umutulupinar <umutulupinarrr@gmail.com>
2023-10-19 00:43:55 +0300

    version method is added

--- umut/main.py
+++ umut/main.py
@@ -1,9 +1,15 @@
+import datetime
 def print_conversation(name,number):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, my name is {name} and my number is {number}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+def print_versiyon():
+    if False:
+        crt=datetime.datetime.now()
+    crt='2023-10-19 00:37:50.801773'
+    print(f'{crt}')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print_versiyon()
     print_conversation('umut','009')
