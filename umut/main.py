e5e951dd8afb7c8ea2920c272cd6487b6a6d76ee
umutulupinar <umutulupinarrr@gmail.com>
2023-11-05 15:57:37 +0300

    pyramid method is added

--- umut/main.py
+++ umut/main.py
@@ -13,9 +13,20 @@ def carpim(number1,number2):
     sonuc=number1*number2
     return sonuc
 
+def print_pyramid():
+    taban_uzunlugu = int(input("Piramidin taban uzunluğunu girin: "))
+
+    for i in range(1, taban_uzunlugu + 1):
+        bosluk_sayisi = taban_uzunlugu - i
+        yildiz_sayisi = 2 * i - 1
+        bosluklar = " " * bosluk_sayisi
+        yildizlar = "*" * yildiz_sayisi
+        print(bosluklar + yildizlar)
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_versiyon()
     print_conversation('umut','009')
     carpimSonucu=carpim(4,5)
     print(carpimSonucu)
+    print_pyramid()
